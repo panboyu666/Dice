33d6d2c64a1a1c1513f6bc960a88b56cfe359483
panboyu <lihaoming1215@gmail.com>
2021-06-15 01:29:32 +0800

    one

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+
+
+}
