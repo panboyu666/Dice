1a13b68857145908861ad2c37bc2435eaa31d6fe
panboyu <lihaoming1215@gmail.com>
2021-06-15 17:02:35 +0800

    change

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.HashMap;
 import java.util.LinkedHashMap;
@@ -15,6 +16,7 @@ import java.util.LinkedList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button buttonsignin,buttonsignup;
 
 
     @Override
@@ -22,9 +24,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        buttonsignin =findViewById(R.id.signin);
+        buttonsignup =findViewById(R.id.signup);
     }
 
 
+    public void buttonchangein(View view) {
+        Toast.makeText(this, "你點選了登入", Toast.LENGTH_SHORT).show();
+    }
 
+    public void buttonchangeup(View view) {
+        Toast.makeText(this, "你點選了註冊", Toast.LENGTH_SHORT).show();
+    }
 
 }
