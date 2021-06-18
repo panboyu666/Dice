2d9f21f734762b17c7650289a698347666f584b1
panboyu <lihaoming1215@gmail.com>
2021-06-18 09:45:28 +0800

    change

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,22 +1,15 @@
 package com.example.myapplication;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ListView;
-import android.widget.SimpleAdapter;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.LinkedList;
 
 public class MainActivity extends AppCompatActivity {
-
-    private Button buttonsignin,buttonsignup;
 
 
     @Override
@@ -24,17 +17,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        buttonsignin =findViewById(R.id.signin);
-        buttonsignup =findViewById(R.id.signup);
+
     }
 
 
-    public void buttonchangein(View view) {
-        Toast.makeText(this, "你點選了登入", Toast.LENGTH_SHORT).show();
+    public void tourist(View view) {
+        Intent intent = new Intent(this,MainActivity2.class);
+        startActivity(intent);
     }
 
-    public void buttonchangeup(View view) {
-        Toast.makeText(this, "你點選了註冊", Toast.LENGTH_SHORT).show();
-    }
 
 }
